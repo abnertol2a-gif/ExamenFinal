b8ac743116ecc57b9124af81bfad6455939d8002
abnertol2a-gif <abnertol2a@gmail.com>
2025-11-28 22:23:22 -0600

    Se agregan datos coherentes y se agrega la pagina de politicas de privacidad

--- WebApplication1/WebApplication1/Models/SeedData.cs
+++ WebApplication1/WebApplication1/Models/SeedData.cs
@@ -28,29 +28,29 @@ public static class SeedData
                     Nombre = "El Señor de los Anillos",
                     Autor = "J.R.R. Tolkien",
                     ReleaseDate = DateTime.Parse("1954-07-29"),
-                    Genre = "Fantasía",
+                    Genre = "Fantasia",
                     Price = 10.99M,
-                    Rating = "123",
+                    Rating = "A",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
                 {
-                    Nombre = "Crónica de una Muerte Anunciada",
-                    Autor = "Gabriel García Márquez",
+                    Nombre = "Cronica de una Muerte Anunciada",
+                    Autor = "Gabriel Garcia Marquez",
                     ReleaseDate = DateTime.Parse("1981-03-15"),
-                    Genre = "Realismo Mágico",
+                    Genre = "Realismo Magico",
                     Price = 5.99M,
-                    Rating = "123",
+                    Rating = "B1",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
                 {
                     Nombre = "El Principito",
-                    Autor = "Antoine de Saint-Exupéry",
+                    Autor = "Antoine de Saint Exupery",
                     ReleaseDate = DateTime.Parse("1943-04-06"),
-                    Genre = "Fábula",
+                    Genre = "Fabula",
                     Price = 4.99M,
-                    Rating = "123",
+                    Rating = "A2",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -58,9 +58,9 @@ public static class SeedData
                     Nombre = "Fahrenheit 451",
                     Autor = "Ray Bradbury",
                     ReleaseDate = DateTime.Parse("1953-10-19"),
-                    Genre = "Distopía",
+                    Genre = "Distopia",
                     Price = 6.99M,
-                    Rating = "123",
+                    Rating = "C",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -68,9 +68,9 @@ public static class SeedData
                     Nombre = "El Hobbit",
                     Autor = "J.R.R. Tolkien",
                     ReleaseDate = DateTime.Parse("1937-09-21"),
-                    Genre = "Fantasía",
+                    Genre = "Fantasia",
                     Price = 7.49M,
-                    Rating = "123",
+                    Rating = "A3",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -80,17 +80,17 @@ public static class SeedData
                     ReleaseDate = DateTime.Parse("1813-01-28"),
                     Genre = "Romance",
                     Price = 4.49M,
-                    Rating = "123",
+                    Rating = "B",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
                 {
-                    Nombre = "Drácula",
+                    Nombre = "Dracula",
                     Autor = "Bram Stoker",
                     ReleaseDate = DateTime.Parse("1897-05-26"),
                     Genre = "Terror",
                     Price = 5.49M,
-                    Rating = "123",
+                    Rating = "T",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -100,17 +100,17 @@ public static class SeedData
                     ReleaseDate = DateTime.Parse("1986-09-15"),
                     Genre = "Terror",
                     Price = 9.49M,
-                    Rating = "123",
+                    Rating = "T1",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
                 {
-                    Nombre = "El Código Da Vinci",
+                    Nombre = "El Codigo Da Vinci",
                     Autor = "Dan Brown",
                     ReleaseDate = DateTime.Parse("2003-03-18"),
                     Genre = "Suspenso",
                     Price = 7.99M,
-                    Rating = "123",
+                    Rating = "S",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -118,9 +118,9 @@ public static class SeedData
                     Nombre = "Los Juegos del Hambre",
                     Autor = "Suzanne Collins",
                     ReleaseDate = DateTime.Parse("2008-09-14"),
-                    Genre = "Distopía",
+                    Genre = "Distopia",
                     Price = 8.49M,
-                    Rating = "123",
+                    Rating = "D1",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -128,9 +128,9 @@ public static class SeedData
                     Nombre = "En Llamas",
                     Autor = "Suzanne Collins",
                     ReleaseDate = DateTime.Parse("2009-09-01"),
-                    Genre = "Distopía",
+                    Genre = "Distopia",
                     Price = 8.49M,
-                    Rating = "123",
+                    Rating = "D2",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -138,9 +138,9 @@ public static class SeedData
                     Nombre = "Sinsajo",
                     Autor = "Suzanne Collins",
                     ReleaseDate = DateTime.Parse("2010-08-24"),
-                    Genre = "Distopía",
+                    Genre = "Distopia",
                     Price = 8.49M,
-                    Rating = "123",
+                    Rating = "D3",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -148,9 +148,9 @@ public static class SeedData
                     Nombre = "El Alquimista",
                     Autor = "Paulo Coelho",
                     ReleaseDate = DateTime.Parse("1988-01-01"),
-                    Genre = "Ficción Filosófica",
+                    Genre = "Ficcion Filosofica",
                     Price = 5.99M,
-                    Rating = "123",
+                    Rating = "F1",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -160,7 +160,7 @@ public static class SeedData
                     ReleaseDate = DateTime.Parse("2002-01-01"),
                     Genre = "Thriller",
                     Price = 7.99M,
-                    Rating = "123",
+                    Rating = "T2",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -168,9 +168,9 @@ public static class SeedData
                     Nombre = "Dune",
                     Autor = "Frank Herbert",
                     ReleaseDate = DateTime.Parse("1965-08-01"),
-                    Genre = "Ciencia Ficción",
+                    Genre = "Ciencia Ficcion",
                     Price = 9.99M,
-                    Rating = "123",
+                    Rating = "C1",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -178,9 +178,9 @@ public static class SeedData
                     Nombre = "Neuromante",
                     Autor = "William Gibson",
                     ReleaseDate = DateTime.Parse("1984-07-01"),
-                    Genre = "Ciencia Ficción",
+                    Genre = "Ciencia Ficcion",
                     Price = 8.49M,
-                    Rating = "123",
+                    Rating = "C2",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -190,7 +190,7 @@ public static class SeedData
                     ReleaseDate = DateTime.Parse("2015-01-13"),
                     Genre = "Suspenso",
                     Price = 7.99M,
-                    Rating = "123",
+                    Rating = "S1",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -198,9 +198,9 @@ public static class SeedData
                     Nombre = "Ready Player One",
                     Autor = "Ernest Cline",
                     ReleaseDate = DateTime.Parse("2011-08-16"),
-                    Genre = "Ciencia Ficción",
+                    Genre = "Ciencia Ficcion",
                     Price = 8.99M,
-                    Rating = "123",
+                    Rating = "C3",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -208,19 +208,19 @@ public static class SeedData
                     Nombre = "Harry Potter y la Piedra Filosofal",
                     Autor = "J.K. Rowling",
                     ReleaseDate = DateTime.Parse("1997-06-26"),
-                    Genre = "Fantasía",
+                    Genre = "Fantasia",
                     Price = 6.99M,
-                    Rating = "123",
+                    Rating = "HP1",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
                 {
-                    Nombre = "Harry Potter y la Cámara Secreta",
+                    Nombre = "Harry Potter y la Camara Secreta",
                     Autor = "J.K. Rowling",
                     ReleaseDate = DateTime.Parse("1998-07-02"),
-                    Genre = "Fantasía",
+                    Genre = "Fantasia",
                     Price = 6.99M,
-                    Rating = "123",
+                    Rating = "HP2",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -228,39 +228,39 @@ public static class SeedData
                     Nombre = "Harry Potter y el Prisionero de Azkaban",
                     Autor = "J.K. Rowling",
                     ReleaseDate = DateTime.Parse("1999-07-08"),
-                    Genre = "Fantasía",
+                    Genre = "Fantasia",
                     Price = 7.49M,
-                    Rating = "123",
+                    Rating = "HP3",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
                 {
-                    Nombre = "Harry Potter y el Cáliz de Fuego",
+                    Nombre = "Harry Potter y el Caliz de Fuego",
                     Autor = "J.K. Rowling",
                     ReleaseDate = DateTime.Parse("2000-07-08"),
-                    Genre = "Fantasía",
+                    Genre = "Fantasia",
                     Price = 7.99M,
-                    Rating = "123",
+                    Rating = "HP4",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
                 {
-                    Nombre = "Harry Potter y la Orden del Fénix",
+                    Nombre = "Harry Potter y la Orden del Fenix",
                     Autor = "J.K. Rowling",
                     ReleaseDate = DateTime.Parse("2003-06-21"),
-                    Genre = "Fantasía",
+                    Genre = "Fantasia",
                     Price = 8.49M,
-                    Rating = "123",
+                    Rating = "HP5",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
                 {
-                    Nombre = "Harry Potter y el Misterio del Príncipe",
+                    Nombre = "Harry Potter y el Misterio del Principe",
                     Autor = "J.K. Rowling",
                     ReleaseDate = DateTime.Parse("2005-07-16"),
-                    Genre = "Fantasía",
+                    Genre = "Fantasia",
                     Price = 8.49M,
-                    Rating = "123",
+                    Rating = "HP6",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -268,9 +268,9 @@ public static class SeedData
                     Nombre = "Harry Potter y las Reliquias de la Muerte",
                     Autor = "J.K. Rowling",
                     ReleaseDate = DateTime.Parse("2007-07-21"),
-                    Genre = "Fantasía",
+                    Genre = "Fantasia",
                     Price = 9.49M,
-                    Rating = "123",
+                    Rating = "HP7",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -278,19 +278,19 @@ public static class SeedData
                     Nombre = "El Silmarillion",
                     Autor = "J.R.R. Tolkien",
                     ReleaseDate = DateTime.Parse("1977-09-15"),
-                    Genre = "Fantasía",
+                    Genre = "Fantasia",
                     Price = 8.99M,
-                    Rating = "123",
+                    Rating = "A4",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
                 {
                     Nombre = "El Perfume",
-                    Autor = "Patrick Süskind",
+                    Autor = "Patrick Suskind",
                     ReleaseDate = DateTime.Parse("1985-10-01"),
-                    Genre = "Ficción",
+                    Genre = "Ficcion",
                     Price = 6.49M,
-                    Rating = "123",
+                    Rating = "F2",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -298,9 +298,9 @@ public static class SeedData
                     Nombre = "La Metamorfosis",
                     Autor = "Franz Kafka",
                     ReleaseDate = DateTime.Parse("1915-10-01"),
-                    Genre = "Ficción",
+                    Genre = "Ficcion",
                     Price = 4.49M,
-                    Rating = "123",
+                    Rating = "F3",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -310,7 +310,7 @@ public static class SeedData
                     ReleaseDate = DateTime.Parse("1862-01-01"),
                     Genre = "Drama",
                     Price = 7.99M,
-                    Rating = "123",
+                    Rating = "D4",
                     Estado = EstadoLibro.Disponible
                 },
                 new Libro
@@ -318,11 +318,12 @@ public static class SeedData
                     Nombre = "El Retrato de Dorian Gray",
                     Autor = "Oscar Wilde",
                     ReleaseDate = DateTime.Parse("1890-07-01"),
-                    Genre = "Ficción",
+                    Genre = "Ficcion",
                     Price = 5.49M,
-                    Rating = "123",
+                    Rating = "F4",
                     Estado = EstadoLibro.Disponible
                 }
+
 
             );
             context.SaveChanges();
